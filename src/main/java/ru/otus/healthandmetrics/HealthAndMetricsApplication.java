63480d4a8c2107e773c3b496e8462ce58a92062d
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2021-11-02 22:04:48 +0600

    Initial

---
+++ src/main/java/ru/otus/healthandmetrics/HealthAndMetricsApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.healthandmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAndMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthAndMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/otus/healthandmetrics/HealthAndMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.healthandmetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthAndMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
