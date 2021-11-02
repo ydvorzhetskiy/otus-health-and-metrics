69edb3e8a07a9ee68493bcb1a28c9a0f4e66dd4d
Yuriy Dvorzhetskiy <dvorzhetskij@mail.ru>
2021-11-03 05:46:57 +0600

    Small fixes

--- src/main/java/ru/otus/healthandmetrics/rest/MetricsCounter.java
+++ src/main/java/ru/otus/healthandmetrics/rest/MetricsCounter.java
@@ -1,7 +1,9 @@
 package ru.otus.healthandmetrics.rest;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +15,12 @@ public class MetricsCounter {
 
     @GetMapping("/counter")
     public void counter() {
-        registry.counter("counter.example");
+        registry.counter("counter.example", Tags.empty())
+            .increment();
     }
 
     @GetMapping("/gauge")
     public void gauge() {
-        registry.gauge("counter.example", Math.random());
+        registry.gauge("gauge.example", Math.random());
     }
 }
